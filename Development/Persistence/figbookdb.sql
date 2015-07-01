f762a2b6bb6c6b3293934e3e06b42761c4f5780e
Armand Pieterse <u12167844@tuks.co.za>
2015-07-02 01:47:53 +0200

    fixed the sql file
    some minor errors found in the sql file

--- Development/Persistence/figbookdb.sql
+++ Development/Persistence/figbookdb.sql
@@ -6,7 +6,7 @@
 -- Generation Time: Jul 01, 2015 at 03:15 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
-
+use figbookdb;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -19,73 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `figbookdb`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `author`
---
-
-CREATE TABLE IF NOT EXISTS `author` (
-  `AuthorID` int(11) NOT NULL AUTO_INCREMENT,
-  `UserID` int(11) NOT NULL,
-  `ManuscriptID` int(11) NOT NULL,
-  `UserRoleID` int(11) NOT NULL,
-  PRIMARY KEY (`AuthorID`),
-  FOREIGN KEY (`UserID`) REFERENCES useraccount(`UserID`),
-  FOREIGN KEY (`ManuscriptID`) REFERENCES manuscript(`ManuscriptID`),
-  FOREIGN KEY (`UserRoleID`) REFERENCES userrole(`UserRoleID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comment`
---
-
-CREATE TABLE IF NOT EXISTS `comment` (
-  `CommentID` int(11) NOT NULL AUTO_INCREMENT,
-  `Title` int(11) NOT NULL,
-  `Body` longblob NOT NULL,
-  `UserID` int(11) NOT NULL,
-  `ManuscriptID` int(11) NOT NULL,
-  PRIMARY KEY (`CommentID`),
-  FOREIGN KEY (`ManuscriptID`) REFERENCES manuscript(`ManuscriptID`),
-  FOREIGN KEY (`UserID`) REFERENCES userrole(`UserID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `letter`
---
-
-CREATE TABLE IF NOT EXISTS `letter` (
-  `LetterID` int(11) NOT NULL AUTO_INCREMENT,
-  `Title` varchar(140) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Body` longblob NOT NULL,
-  `LetterType` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`LetterID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `manuscript`
---
-
-CREATE TABLE IF NOT EXISTS `manuscript` (
-  `ManuscriptID` int(11) NOT NULL AUTO_INCREMENT,
-  `Title` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Date` date NOT NULL,
-  `ISBN` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Body` longblob NOT NULL,
-  `UserID` int(11) NOT NULL,
-  `Status` int(11) NOT NULL,
-  `Genre` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`ManuscriptID`),
-  FOREIGN KEY (`UserID`) REFERENCES userrole(`UserID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -105,6 +38,21 @@ CREATE TABLE IF NOT EXISTS `useraccount` (
 
 -- --------------------------------------------------------
 
+
+--
+-- Table structure for table `letter`
+--
+
+CREATE TABLE IF NOT EXISTS `letter` (
+  `LetterID` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(140) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Body` longblob NOT NULL,
+  `LetterType` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`LetterID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `userrole`
 --
@@ -119,3 +67,64 @@ CREATE TABLE IF NOT EXISTS `userrole` (
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+--
+-- Table structure for table `manuscript`
+--
+
+CREATE TABLE IF NOT EXISTS `manuscript` (
+  `ManuscriptID` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Date` date NOT NULL,
+  `ISBN` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Body` longblob NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Status` int(11) NOT NULL,
+  `Genre` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`ManuscriptID`),
+  FOREIGN KEY (`UserID`) REFERENCES userrole(`UserRoleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+CREATE TABLE IF NOT EXISTS `author` (
+  `AuthorID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserID` int(11) NOT NULL,
+  `ManuscriptID` int(11) NOT NULL,
+  `UserRoleID` int(11) NOT NULL,
+  PRIMARY KEY (`AuthorID`),
+  FOREIGN KEY (UserID) REFERENCES useraccount(UserID),
+  FOREIGN KEY (ManuscriptID) REFERENCES manuscript(ManuscriptID),
+  FOREIGN KEY (UserRoleID) REFERENCES userrole(UserRoleID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `CommentID` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` int(11) NOT NULL,
+  `Body` longblob NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `ManuscriptID` int(11) NOT NULL,
+  PRIMARY KEY (`CommentID`),
+  FOREIGN KEY (`ManuscriptID`) REFERENCES manuscript(`ManuscriptID`),
+  FOREIGN KEY (`UserID`) REFERENCES userrole(`UserRoleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+
+
+
+
+
