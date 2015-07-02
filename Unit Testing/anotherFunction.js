0e6132edfaaa49bcf8b2123170b92d03446db644
Jimmy <jimmypeleha@yahoo.com>
2015-07-02 15:51:21 +0200

    Example For QUnit Object

--- Unit Testing/anotherFunction.js
+++ Unit Testing/anotherFunction.js
@@ -1,3 +1,8 @@
 var registration = function(){
 	// body...
 }
+
+function multiply(x,y){
+	document.getElementById('result').innerHTML = x*y;
+	return x*y;
+}
--- Unit Testing/firstTest.js
+++ Unit Testing/firstTest.js
@@ -3,8 +3,12 @@
 QUnit.test("register test", function(assert){
 	//ok(registration, "exists")
 	function f(){
-		assert.equal(registration, registration);
+		//testing the type a variable
 		assert.equal(typeof registration === 'function', true);
+		//testing whether your function returns the expected value
+		assert.equal(multiply(2,2), 4);
+		//testing whether HTML elements are as expected
+		assert.equal(document.getElementById("result").innerHTML, "4");
 	}
 	f();
 });
