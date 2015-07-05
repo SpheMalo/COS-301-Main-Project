46ac7827b1ae118407eb6a16a81e82bcd474c6db
Sphe Malo <ssmalo2@gmail.com>
2015-07-05 16:30:13 +0200

    Javascript password validation on register form complete

--- scripts/register.js
+++ scripts/register.js
@@ -7,19 +7,57 @@ window.onload = function()
 			//$('#register-button').attr('disabled', true);
 			//$('#register-button').attr('value','..Processing..');
 			
-			var UserInfo = {
-				"username" : document.getElementById("username").value,
-				"password" : document.getElementById("password").value,
-				"role" : document.getElementById("roleSelect").value,			
-				"email" : document.getElementById("email").value,
-				"action" : "register"
-			}
-			//alert(UserInfo.role);
-			var JSONstring = JSON.stringify(UserInfo);
-			ajaxRegisterFunction(JSONstring);
+			var pass1 = document.getElementById('password');
+			var pass2 = document.getElementById('confirmpassword');
 			
-			event.preventDefault();
+			if (pass2.value != pass1.value)
+			{
+				if (document.getElementById('incorrectVal') == null) {
+					var form = document.getElementById("regDiv");
+				
+					var incorrectVal = document.createElement('p');
+					incorrectVal.id = "incorrectVal";
+					incorrectVal.innerHTML = "Passwords do not match.";
+					incorrectVal.style.color = "#F95050";
+					incorrectVal.style.fontSize ="18pt";
+					form.appendChild(incorrectVal);
+				}
+				
+				
+				
+				pass2.value = '';
+				pass1.value = '';
+				pass2.style.backgroundColor = "#F95050";
+				pass1.style.backgroundColor = "#F95050";
+				
+				pass1.onfocus = function (){pass1.style.backgroundColor = "white";};
+				pass2.onfocus = function (){pass2.style.backgroundColor = "white";};
+				
+				pass1.onblur = function (){pass1.style.backgroundColor = "#ABD1BC";};
+				pass2.onblur = function (){pass2.style.backgroundColor = "#ABD1BC";};
+				
+				event.preventDefault();
+			}
+			else
+			{
+				var incorrectVal = document.getElementById('incorrectVal');
+				incorrectVal.innerHTML = "";
+				
+				var UserInfo = {
+					"username" : document.getElementById("username").value,
+					"password" : document.getElementById("password").value,
+					"role" : document.getElementById("roleSelect").value,			
+					"email" : document.getElementById("email").value,
+					"action" : "register"
+				}
+				//alert(UserInfo.role);
+				var JSONstring = JSON.stringify(UserInfo);
+				ajaxRegisterFunction(JSONstring);
+				
+				event.preventDefault();
+			}
 		});
+		
 		
 		$( "#login-button" ).click(function() {
 			var UserInfo = {
