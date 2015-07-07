b50c7ecd915bfb6b8e8cd408c0bef7b569ad7e89
Jimmy <jimmypeleha@yahoo.com>
2015-07-07 14:35:51 +0200

    QUnit

--- Unit Testing/firstTest.js
+++ Unit Testing/firstTest.js
@@ -1,14 +1,9 @@
 /*This test is for the setting up of the Qunit framework*/
 
 QUnit.test("register test", function(assert){
-	//ok(registration, "exists")
 	function f(){
-		//testing the type a variable
-		assert.equal(typeof registration === 'function', true);
-		//testing whether your function returns the expected value
-		assert.equal(multiply(2,2), 4);
-		//testing whether HTML elements are as expected
-		assert.equal(document.getElementById("result").innerHTML, "4");
-	}
+			assert.equal(typeof document, 'object');
+			assert.equal(typeof ajaxFunction, 'function');
+		}
 	f();
 });
