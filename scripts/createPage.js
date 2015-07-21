81c05491ffa6cba697fdb3a264c0a3e664c3df42
Sphe Malo <ssmalo2@gmail.com>
2015-07-21 21:26:03 +0200

    create/retrieve edit timestamp

--- scripts/createPage.js
+++ scripts/createPage.js
@@ -1,7 +1,82 @@
 function editSection(value)
 {
-	alert(value);
+
+//	var editTimeStamp;
+//	var title = $("#pageSelect :selected").html();
+//	var content = document.getElementById("editor").value;
+//	var sectionHeading = $("#pageEditTitle").html();
+//	var actualContent = "="+sectionHeading+"= \n"+content;
+	var jsonString ='{'
+					+'"format": "json",'
+					+'"action": "getTimeStamp",'
+					+'"title": "Groceries",'
+					+'"section": '+value
+				    +'}';
+	$.ajax({
+				url: "scripts/FigbookActionHandler/actionHandler.php",
+				data: "json="+jsonString,
+				dataType: 'json',
+				type: 'POST',
+				success: function (data) {
+					localStorage.setItem("tStamp", data.date_last_edited);
+				},
+				error: function (data) {
+					console.log('Error: Request failed. ' + (data.responseText));
 	
+				}
+			});
+	
+	/* $.post("scripts/mediawiki/api.php?action=query&prop=revisions&titles="+title+"&rvprop=timestamp|content&rvsection="+section+"&format=json", function(data){
+		//alert(JSON.stringify(data));
+		
+		var string = JSON.stringify(data);
+        var tokens = string.split("\"");
+        var index = tokens[5];
+       editTimeStamp = data.query.pages[index].revisions[0].timestamp;
+	   
+	localStorage.setItem("tStamp", editTimeStamp);
+	
+
+	 });*/
+	/*$("#viewPage").hide(900);
+	$("#pageList").hide(700);
+	$("#createPage").hide(500);
+	$("#Dummy").hide(400);
+	$("#Page").hide(400);
+	$("#editSection").css("visibility", "visible");
+	$("#pageEditTitle").html($("#"+section+" .mw-headline").html());
+	$("#editor").html("");
+
+	/*var page = document.getElementById("Page").nextSibling;
+		var links = page.(childNodes);
+		
+		var num_links =1;
+		for(var i=0; i<links.length; i++) {
+			if(links[i].innerHTML == "edit")
+			{
+				links[i].setAttribute('href', "#");
+				links[i].setAttribute('onclick', "editSection("+num_links+")");
+				num_links++;
+			}
+		}*/
+
+	//alert(document.getElementById(""+section).nextSibling);
+	/*var arr = $("#"+section).nextUntil("h1");
+	str = "";
+	for(var i = 0; i < arr.length; i++){
+		if (arr[i].nodeName == "P") {
+			
+			str+= arr[i].innerHTML+"\n";
+		}
+	}
+	
+	var res = str.replace("<br>", "\n");
+	
+	//alert($("#"+section).nextUnitl().html());
+	$("#editor").html(res);
+	$("#saveBtn").attr("name","" + section);
+	
+	*/
 }
 
 		
--- scripts/saveTextToDB.js
+++ scripts/saveTextToDB.js
@@ -1,48 +1,65 @@
 function saveText()
 {
+    
+    //Getting info to pass to the ajax function
     var title = $("#pageSelect :selected").html();
     var sectionHeading = $("#pageEditTitle").html();
     var sectionNumber = $("#saveBtn").attr("name");
     var content = document.getElementById("editor").value;
     sectionNumber = parseInt(sectionNumber);
     
-
+    var timeStamp = localStorage.getItem("tStamp");
     
-    $.post("scripts/mediawiki/api.php?action=query&prop=info|revisions&meta=tokens&rvprop=timestamp&titles="+title+"&format=json", function(data){
-        var string = JSON.stringify(data);
-        var tokens = string.split("\"");
-        var index = tokens[5];
-        var timeStamp = localStorage.getItem("tStamp");
-        
-        $.ajax({
-                    url: "scripts/mediawiki/api.php",
-                    data: {
-                        format: 'json',
-                        action: 'edit',
-                        title: title,
-                        section: sectionNumber,
-                        text:"="+sectionHeading+"= \n" + content,
-                        token: data.query.tokens.csrftoken
-                    },
-                    dataType: 'json',
-                    type: 'POST',
-                    success: function (data) {
-                        if (data && data.edit && data.edit.result === 'Success') {
-                            alert("Successful");
-                           alert(JSON.stringify(data));
-                        } else if (data && data.error) {
-                            alert('Error: API returned error code "' + data.error.code + '": ' + data.error.info);
-                        } else {
-                            alert('Error: Unknown result from API.');
-                        }
-                    },
-                    error: function (data) {
-                        console.log('Error: Request failed. ' + JSON.stringify(data));
-                        $('#Page').append("<a href='/scripts/mediawiki/index.php/" + params.title + "'>Link to your book</a>");
-
-                        event.preventDefault();
-                       // get_page(params);
+    var jsonString ='{'
+					+'"format": "json",'
+					+'"action": "verifyTimeStamp",'
+					+'"title": "Groceries",'
+					+'"section": '+value
+				    +'}';
+                    
+    $.ajax({
+				url: "scripts/FigbookActionHandler/actionHandler.php",
+				data: "json="+jsonString,
+				dataType: 'json',
+				type: 'POST',
+				success: function (data) {
+                    
+				},
+				error: function (data) {
+					console.log('Error: Request failed. ' + (data.responseText));
+	
+				}
+			});
+    
+    //if no conflict, send to api
+    $.ajax({
+                url: "scripts/mediawiki/api.php",
+                data: {
+                    format: 'json',
+                    action: 'edit',
+                    title: title,
+                    section: sectionNumber,
+                    text:"="+sectionHeading+"= \n" + content,
+                    token: data.query.tokens.csrftoken
+                },
+                dataType: 'json',
+                type: 'POST',
+                success: function (data) {
+                    if (data && data.edit && data.edit.result === 'Success') {
+                        alert("Successful");
+                       alert(JSON.stringify(data));
+                    } else if (data && data.error) {
+                        alert('Error: API returned error code "' + data.error.code + '": ' + data.error.info);
+                    } else {
+                        alert('Error: Unknown result from API.');
                     }
-                });
-        });
+                },
+                error: function (data) {
+                    console.log('Error: Request failed. ' + JSON.stringify(data));
+                    $('#Page').append("<a href='/scripts/mediawiki/index.php/" + params.title + "'>Link to your book</a>");
+
+                    event.preventDefault();
+                   // get_page(params);
+                }
+            });
 }
