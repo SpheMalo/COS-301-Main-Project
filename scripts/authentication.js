2ea4089e3a71c81d249016b315bd5bed1d27d442
kgomotso-sito <khomcy.sito@gmail.com>
2015-07-28 23:04:23 +0200

    Structured code and Added textarea with formatting options

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,12 +1,8 @@
 window.onload = function()
 {
-
-
 	$(document).ready(function(){
 		$('#register-button').click(function(){
-			//$('#register-button').attr('disabled', true);
-			//$('#register-button').attr('value','..Processing..');
-			 
+
 			var pass1 = document.getElementById('password');
 			var pass2 = document.getElementById('confirmpassword');
 			
@@ -25,8 +21,7 @@ window.onload = function()
 			{
 				event.preventDefault();
 				var incorrectVal = document.getElementById('incorrectVal');
-				//incorrectVal.innerHTML = "";
-				
+
 				var UserInfo = {
 					"username" : document.getElementById("username").value,
 					"password" : document.getElementById("password").value,
@@ -37,7 +32,6 @@ window.onload = function()
 				
 				var stat = 1;
 				wiki_createAccount(UserInfo.username,UserInfo.password,UserInfo.email, UserInfo.role, stat);
-				
 				event.preventDefault();
 			}
 		});
@@ -52,105 +46,98 @@ window.onload = function()
 			if (field == "invalidemailaddress") {
 				errCode = "Invalid email address. Please re-enter email address";
 			}
-				if (document.getElementById('incorrectVal') == null) {
-					var form = document.getElementById("regDiv");
+
+			if (document.getElementById('incorrectVal') == null) {
+				var form = document.getElementById("regDiv");
+				var incorrectVal = document.createElement('p');
+				incorrectVal.id = "incorrectVal";
+				incorrectVal.innerHTML = errCode;
+				incorrectVal.style.color = "#F95050";
+				incorrectVal.style.fontSize ="18pt";
+				form.appendChild(incorrectVal);
+			}
+			
+			else
+			{	
+				var incorrectVal = document.getElementById('incorrectVal');
+				incorrectVal.innerHTML = errCode;
+			}
+				
+			if (field == "password" || field == "password-name-match")
+			{
+				var pass1 = document.getElementById('password');
+				var pass2 = document.getElementById('confirmpassword');
+			
+				pass2.value = '';
+				pass1.value = '';
+				pass2.style.backgroundColor = "#F95050";
+				pass1.style.backgroundColor = "#F95050";
+				
+				pass1.onfocus = function (){pass1.style.backgroundColor = "white";};
+				pass2.onfocus = function (){pass2.style.backgroundColor = "white";};
+				
+				pass1.onblur = function (){pass1.style.backgroundColor = "#ABD1BC";};
+				pass2.onblur = function (){pass2.style.backgroundColor = "#ABD1BC";};
+			}
+			else if (field == "invalidemailaddress")
+			{
+				var email = document.getElementById('email');
+				
+				email.value = '';
+				email.style.backgroundColor = "#F95050";
+				
+				email.onfocus = function (){email.style.backgroundColor = "white";};
+				
+				email.onblur = function (){email.style.backgroundColor = "#ABD1BC";};
+			}	
+			else if (field == "userexists")
+			{
+				var username = document.getElementById('username');
+				
+				username.value = '';
+				username.style.backgroundColor = "#F95050";
+				
+				username.onfocus = function (){username.style.backgroundColor = "white";};
+				
+				username.onblur = function (){username.style.backgroundColor = "#ABD1BC";};
+			}
+			else if (field == "loginErr")
+			{
+				if (document.getElementById('loginIncorrect') == null)
+				{
+					var form = document.getElementById("logDiv");
 				
 					var incorrectVal = document.createElement('p');
-					incorrectVal.id = "incorrectVal";
+					incorrectVal.id = "loginIncorrect";
 					incorrectVal.innerHTML = errCode;
 					incorrectVal.style.color = "#F95050";
 					incorrectVal.style.fontSize ="18pt";
 					form.appendChild(incorrectVal);
 				}
-				
 				else
 				{	
-					var incorrectVal = document.getElementById('incorrectVal');
+					var incorrectVal = document.getElementById('loginIncorrect');
 					incorrectVal.innerHTML = errCode;
 				}
 				
-				if (field == "password" || field == "password-name-match")
-				{
-					var pass1 = document.getElementById('password');
-					var pass2 = document.getElementById('confirmpassword');
+				var uname = document.getElementById('LoginUsername');
+				var pword = document.getElementById('LoginPassword');
 				
-					pass2.value = '';
-					pass1.value = '';
-					pass2.style.backgroundColor = "#F95050";
-					pass1.style.backgroundColor = "#F95050";
-					
-					pass1.onfocus = function (){pass1.style.backgroundColor = "white";};
-					pass2.onfocus = function (){pass2.style.backgroundColor = "white";};
-					
-					pass1.onblur = function (){pass1.style.backgroundColor = "#ABD1BC";};
-					pass2.onblur = function (){pass2.style.backgroundColor = "#ABD1BC";};
-				}
+				uname.value = '';
+				pword.value = '';
 				
-				else if (field == "invalidemailaddress")
-				{
-					var email = document.getElementById('email');
-					
-					email.value = '';
-					email.style.backgroundColor = "#F95050";
-					
-					email.onfocus = function (){email.style.backgroundColor = "white";};
-					
-					email.onblur = function (){email.style.backgroundColor = "#ABD1BC";};
-				}
+				uname.style.backgroundColor = "#F95050";
+				pword.style.backgroundColor = "#F95050";
 				
-				else if (field == "userexists")
-				{
-					var username = document.getElementById('username');
-					
-					username.value = '';
-					username.style.backgroundColor = "#F95050";
-					
-					username.onfocus = function (){username.style.backgroundColor = "white";};
-					
-					username.onblur = function (){username.style.backgroundColor = "#ABD1BC";};
-				}
+				uname.onfocus = function (){username.style.backgroundColor = "white";};
+				pword.onfocus = function (){password.style.backgroundColor = "white";};
 				
-				else if (field == "loginErr")
-				{
-					if (document.getElementById('loginIncorrect') == null)
-					{
-						var form = document.getElementById("logDiv");
-					
-						var incorrectVal = document.createElement('p');
-						incorrectVal.id = "loginIncorrect";
-						incorrectVal.innerHTML = errCode;
-						incorrectVal.style.color = "#F95050";
-						incorrectVal.style.fontSize ="18pt";
-						form.appendChild(incorrectVal);
-					}
-				
-					else
-					{	
-						var incorrectVal = document.getElementById('loginIncorrect');
-						incorrectVal.innerHTML = errCode;
-					}
-					
-					var uname = document.getElementById('LoginUsername');
-					var pword = document.getElementById('LoginPassword');
-					
-					uname.value = '';
-					pword.value = '';
-					
-					uname.style.backgroundColor = "#F95050";
-					pword.style.backgroundColor = "#F95050";
-					
-					uname.onfocus = function (){username.style.backgroundColor = "white";};
-					pword.onfocus = function (){password.style.backgroundColor = "white";};
-					
-					uname.onblur = function (){username.style.backgroundColor = "#ABD1BC";};
-					pword.onblur = function (){password.style.backgroundColor = "#ABD1BC";};
-				}
-				
-				
+				uname.onblur = function (){username.style.backgroundColor = "#ABD1BC";};
+				pword.onblur = function (){password.style.backgroundColor = "#ABD1BC";};
+			}
 		}
 		
-		$('#logoutDiv').click(function(){wiki_logout();  });
-		
+		$('#logoutDiv').click(function(){wiki_logout(); });
 		
 		$( "#login-button" ).click(function() {
 			var UserInfo = {
@@ -159,161 +146,103 @@ window.onload = function()
 				"action" : "login"
 			}
 			var JSONstring = JSON.stringify(UserInfo);
-			//alert(JSONstring);
-			
 			ajaxLoginFunction(UserInfo);
-			
 			event.preventDefault();
 		});
-		
-		
-		
-		
-		
-		function ajaxLoginFunction(UserInfo){
-			//alert("Function called ");
-			
-			wiki_auth(UserInfo.username,UserInfo.password,"insideContent.php");
-			
-			/*$.ajax({
-				//url: 'scripts/FigbookActionHandler/actionHandler.php',
-				url: 'scripts/mediawiki/api.php',
-				data: 'json='+JSONstring,
-				dataType: 'json',
-				success: function(data){
-					
-					if(data == "correct")
-					{
-						alert("Logging in...");
-						 event.preventDefault();					
-						window.location.pathname = "FigbookHtml/inside.html";
-					}
-					else if(data == "incorrect")
-					{
-						
-						var pass1 = document.getElementById('LoginUsername');
-						var pass2 = document.getElementById('LoginPassword');
-						
-						//alert("Incorrect credentials");
-						if (document.getElementById('incorrectVal') == null) {
-						var form = document.getElementById("logDiv");
-					
-						var incorrectVal = document.createElement('p');
-						incorrectVal.id = "incorrectVal";
-						incorrectVal.innerHTML = "Incorrect credentials.";
-						incorrectVal.style.color = "#F95050";
-						incorrectVal.style.fontSize ="18pt";
-						form.appendChild(incorrectVal);
-						}
-						
-						
-						
-						pass2.value = '';
-						pass1.value = '';
-						pass2.style.backgroundColor = "#F95050";
-						pass1.style.backgroundColor = "#F95050";
-						
-						pass1.onfocus = function (){pass1.style.backgroundColor = "white";};
-						pass2.onfocus = function (){pass2.style.backgroundColor = "white";};
-						
-						pass1.onblur = function (){pass1.style.backgroundColor = "#ABD1BC";};
-						pass2.onblur = function (){pass2.style.backgroundColor = "#ABD1BC";};
-						
-						event.preventDefault();
-								
-					}
-					else if(data == "not_active"){
-					    
-						alert("User is Deactivated");
-					}
-					},
-				error: function(data){
-					alert("error :"+data.responseText);
-				}		
-			});*/
-		}
-		
-		
-		
-		//This is the function that uses wikis create account api
-		function wiki_createAccount(username, password, email, role, status)
-			  {			
-				  //alert("I get here");
-				$.post('scripts/mediawiki/api.php?action=createaccount&name=' + username + 
-				    '&password=' + password + '&uRole='+ role + '&uStatus=' + status +'&email='+email+'&format=json', function(data) {
 
-				if(data.createaccount.result === 'NeedToken') {
-					
-				    $.post('scripts/mediawiki/api.php?action=createaccount&name=' + username + '&email='+email +'&realname=test'+ 
-					    '&password=' + password +'&uRole='+ role + '&uStatus=' +status+ '&token='+data.createaccount.token+'&format=json', 
-					    function(data) {
-					if(!data.error){
-					   if (data.createaccount.result === "Success") { 
-						   //alert(data.login.sessionid);
-						   //document.location.href=ref; 
-						   alert("You have successfully registered, you can now log in.");
-						    window.location.href = "";
-						   
-						  console.log("Succesfully registered");
-					   } else {
-						console.log('Result: '+ data.createaccount.result);
-					   }
-					} else {
-						var err = JSON.stringify(data.error);
-						err = JSON.parse(err);
-						addErrorCode(err.info, err.code);
-					   console.log('Error: ' + JSON.stringify(data.error));
+		function ajaxLoginFunction(UserInfo){
+			wiki_auth(UserInfo.username,UserInfo.password,"insideContent.php");
+		}
+
+		/**
+		 * Creates a new account using wikis api
+		 * @param {String} username
+		 * @param {String} password
+		 * @param {String} email
+		 * @param {String} role
+		 * @param {Number} status
+		 */
+		function wiki_createAccount(username, password, email, role, status) {			
+			$.post('scripts/mediawiki/api.php?action=createaccount&name=' + username + 
+			    '&password=' + password + '&uRole='+ role + '&uStatus=' + status +'&email='+email+'&format=json', 
+			    function(data) {	
+					if(data.createaccount.result === 'NeedToken') {
+						$.post('scripts/mediawiki/api.php?action=createaccount&name=' + username + '&email='+email +'&realname=test'+ 
+						    '&password=' + password +'&uRole='+ role + '&uStatus=' +status+ '&token='+data.createaccount.token+'&format=json', 
+						    function(data) {
+								if(!data.error){
+								   if (data.createaccount.result === "Success") { 
+									   //alert(data.login.sessionid);
+									   //document.location.href=ref; 
+									   alert("You have successfully registered, you can now log in.");
+									    window.location.href = "";
+									   
+									  console.log("Succesfully registered");
+								   } else {
+									console.log('Result: '+ data.createaccount.result);
+								   }
+								} else {
+									var err = JSON.stringify(data.error);
+									err = JSON.parse(err);
+									addErrorCode(err.info, err.code);
+								   console.log('Error: ' + JSON.stringify(data.error));
+								}
+					    	}
+					  	);
+					} 
+					else {
+					    console.log('Result: ' + data.createaccount.result);
 					}
-				    });
-				} else {
-				    console.log('Result: ' + data.createaccount.result);
-				}
-				if(data.error) {
-				    console.log('Error: ' + data.error);
-				}
-			    });
-			}
-		
-		//This calls sends a POST to the api.php with the action being login
-		//thus login executing the login API functionality of mediawiki.		
-		function wiki_auth(login, pass, ref)
-			  {
-				  //alert("I get here");
-				$.post('scripts/mediawiki/api.php?action=login&lgname=' + login + 
-				    '&lgpassword=' + pass + '&format=json', function(data) {
-					   // alert(data.login.token);
-					     //alert(JSON.stringify(data));
-				if(data.login.result == 'NeedToken') {
-					
-				    $.post('scripts/mediawiki/api.php?action=login&lgname=' + login + 
-					    '&lgpassword=' + pass + '&lgtoken='+data.login.token+'&format=json', 
-					    function(data) {
-					if(!data.error){
-					   if (data.login.result == "Success") { 
-						   //alert(data.login.sessionid);
-						  console.log(JSON.stringify(data));
-						  document.cookie="username="+data.login.lgusername;
-						  //localStorage.setItem("lgusername", data.login.lgusername);
-						   document.location.href=ref; 
-						
-					   } else {
-						addErrorCode("Invalid Credentials. Please enter valid credentials or Register a new account.", "loginErr");
-						console.log('Result: '+ data.login.result);
-					   }
-					} else {
-						
-					   console.log('Error: ' + data.error);
+
+					if(data.error) {
+					    console.log('Error: ' + data.error);
 					}
-				    });
-				} else {
-				    console.log('Result: ' + data.login.result);
-				}
-				if(data.error) {
-				    console.log('Error: ' + data.error);
-				}
-			    });
-			}
-		
+		    	}
+		    );
+		}
+
+		/**
+		 * This calls sends a POST to the api.php with the action being login thus login executing the login API functionality of mediawiki.	
+		 * @param {String} login
+		 * @param {String} pass
+		 * @param {String} ref
+		 */	
+		function wiki_auth(login, pass, ref){
+			$.post('scripts/mediawiki/api.php?action=login&lgname=' + login + 
+			    '&lgpassword=' + pass + '&format=json', 
+			    function(data) {
+					if(data.login.result == 'NeedToken') {
+			    		$.post('scripts/mediawiki/api.php?action=login&lgname=' + login + 
+				    		'&lgpassword=' + pass + '&lgtoken='+data.login.token+'&format=json', 
+				    		function(data) {
+								if(!data.error){
+								   if (data.login.result == "Success") { 
+									   //alert(data.login.sessionid);
+									  console.log(JSON.stringify(data));
+									  document.cookie="username="+data.login.lgusername;
+									  //localStorage.setItem("lgusername", data.login.lgusername);
+									   document.location.href=ref; 
+									
+								   } else {
+									addErrorCode("Invalid Credentials. Please enter valid credentials or Register a new account.", "loginErr");
+									console.log('Result: '+ data.login.result);
+								   }
+								} 
+								else {
+					   				console.log('Error: ' + data.error);
+								}
+			   				 }
+			   			);
+					} else {
+			    		console.log('Result: ' + data.login.result);
+					}
+
+					if(data.error) {
+					    console.log('Error: ' + data.error);
+					}
+			    }
+			);
+		}
 		
 		$('#activate-button').click(function(){
 			var UserInfo = {
@@ -324,8 +253,6 @@ window.onload = function()
 			ajaxAccountFunction(JSONstring);
 			event.preventDefault();
 		});
-		
-			
 		
 		$('#delete-button').click(function(){
 			alert("here");
@@ -349,7 +276,8 @@ window.onload = function()
 			ajaxAccountFunction(JSONstring);
 			event.preventDefault();
 		});
-		 $('#getStatus-button').click(function(){
+
+		$('#getStatus-button').click(function(){
             var UserInfo = {			
 				"action" : "getUserStatus"
 			}
@@ -358,7 +286,8 @@ window.onload = function()
 			ajaxAccountFunction(JSONstring);
 			event.preventDefault();
             
-            });
+        });
+
 		function ajaxAccountFunction(JSONstring){
 			$.ajax({
 				url: 'scripts/FigbookActionHandler/actionHandler.php',
@@ -373,6 +302,6 @@ window.onload = function()
 			});
 		}
 	});
-	
+
 	loadProfile();
 };
