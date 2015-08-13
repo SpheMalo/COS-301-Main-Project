93bc85d6106490daf41cd4ad122bf433db7c0816
Ndivhuwo <nthambelenindivhuwo@gmail.com>
2015-08-13 10:12:32 +0200

    Revert "made minor change to enable login"
    This reverts commit f7408fc971c8412081859dab35ab439d60fcfe76.

--- scripts/manuscript.js
+++ scripts/manuscript.js
@@ -85,7 +85,7 @@ $(document).ready(function () {
 	$('#next-button').click(function(){
 		//Get the data from inputs
 		info.title = $('#title').val();
-		info.firstname = readCookie('username');
+		info.firstname = $('#firstname').val();
 		info.surname = $('#surname').val();
 		
 		var UserInfo = {
@@ -99,18 +99,6 @@ $(document).ready(function () {
 		
 		
 	});
-        function readCookie(name) {
-	        var nameEQ = name + "=";
-	        var ca = document.cookie.split(';');
-	        for (var i = 0; i < ca.length; i++) {
-	            var c = ca[i];
-	            while (c.charAt(0) == ' ')
-	                c = c.substring(1, c.length);
-	            if (c.indexOf(nameEQ) == 0)
-	                return c.substring(nameEQ.length, c.length);
-	        }
-	        return null;
-	        }
 		
 	function ajaxFunction(JSONstring){
 		
@@ -220,51 +208,25 @@ $(document).ready(function () {
 				html += "<option value='" + data.query.allpages[i].title + "'>" + data.query.allpages[i].title + "</option>";				//alert("ishere");
 						
 				//This is where I load the book titles into block divs...
-					var pageTitle = data.query.allpages[i].title;
-                        var replaced = pageTitle.split(' ').join('_');
-                        var loadPageInfo = {
-                            "action": "checkPagePermissions",
-                            "bookTitle": replaced
-                        };
-
-                        var JSONstring = JSON.stringify(loadPageInfo);
-                        $.post('scripts/FigbookActionHandler/actionHandler.php?json=' + JSONstring, function (data)
-                        {
-                            // console.log(JSON.stringify(data));
-                            if (data === "\"success\"") {
-                                html += "<option value='" + pageTitle + "'>" + pageTitle + "</option>";
-                               
-                                //div.off('click');
-                                //div.onclick=onClickBook(pageTitle, div);
-
-                                $("#bookList").append("<div class='bookItem'>"+pageTitle+"</div>");
-                                $('.bookItem').click(function () {
-                                    //alert("book clicked : "+$(this).html());
-
-                                    var loadPageInfo = {
-                                        "title": $(this).html()
-                                    };
-                                    //alert(loadPageInfo.title);
-                                    localStorage.bookTitle = loadPageInfo.title;
-                                    //alert(localStorage.bookTitle);
-                                    //make sure it gets a title for a book to load
-                                    if (loadPageInfo.title !== "")
-                                    {
-                                        get_page(loadPageInfo);
-                                    }
-
-                                });
-                            }
-                            else {
-                                //localStorage.permission = "false";
-                            }
-                        });
-
+				$("#bookList").append("<div class='bookItem'>"+data.query.allpages[i].title+"</div>");
 			});
 			html += "< /select>";
 			
 			document.getElementById("pageList").innerHTML = html;
-			 
+			 $('.bookItem').click(function(){
+				
+				
+				var loadPageInfo = {
+					"title": $(this).html()
+				};
+				
+				localStorage.bookTitle = loadPageInfo.title;
+				if (loadPageInfo.title !== "")
+				{
+				 get_page(loadPageInfo);
+				}
+				//event.preventDefault();
+			});
         });
 	}
 		
@@ -297,7 +259,6 @@ $(document).ready(function () {
                         if (data && data.edit && data.edit.result === 'Success') {
                             alert("Successfully created: "+params.title);
 							localStorage.bookTitle = params.title;
-                                                        lockBook(params.title);
 							getBooks();//Repopulate list of books before loading page
                             get_page(params);
                         } else if (data && data.error) {
@@ -317,28 +278,6 @@ $(document).ready(function () {
             }
         );
         //event.preventDefault();
-    }
-    
-     function lockBook(bookTitle) {
-        var replaced = bookTitle.split(' ').join('_');
-        var UserInfo = {
-            "action": "lockBook",
-            "bookTitle": replaced
-        }
-        var JSONstring = JSON.stringify(UserInfo);
-        $.ajax({
-            url: 'scripts/FigbookActionHandler/actionHandler.php',
-            data: 'json=' + JSONstring,
-            dataType: 'json',
-            success: function (data)
-            {
-                //alert(JSON.stringify(data));
-            }
-            , error: function (data) {
-                //alert(JSON.stringify(data));
-            }
-        });
-        event.preventDefault();
     }
 
     function get_page(params) {
