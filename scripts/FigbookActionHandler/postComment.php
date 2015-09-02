4f455e41230ce4c0d9058f88db0eee29018d220d
Armand Pieterse <u12167844@tuks.co.za>
2015-09-02 12:14:33 +0200

    seperated title and addchapter button

--- scripts/FigbookActionHandler/postComment.php
+++ scripts/FigbookActionHandler/postComment.php
@@ -16,34 +16,24 @@
 	* Persists comment passed into the database
 	* @param $page_id	ID of the page in the database
 	*/
-	function postComment($page_name, $section_number, $com){
+	function postComment($page_id){
+		$com = test_input($_POST["commentText"]);
 		$db = new databaseHandler();
 		if($db->isConnected()){
-			$insertQuery = "INSERT INTO page_comment (comment, page_name, section_number) VALUES ('" . $com . "', '$page_name', '$section_number')";
+			$insertQuery = "INSERT INTO page_comment (comment, page_id) VALUES ('" . $com . "', '$page_id')";
 			$dbconn = $db->getConnection();
         	$insertQueryResult = mysqli_query($dbconn,$insertQuery);
 
         	if($insertQueryResult){
         		echo "successful";
-        		//return "successful";
         	}
         	else{
         		echo "failed";
         	}
 		}
 		else{
-			echo "FAILED";
+			echo "Failed";
 		}
 	}
-
-	//get json string
-$getJSONObject = $_REQUEST['json'];
-
-$object = json_decode(stripslashes($getJSONObject));
-//get the action flag	
-$pg_name = $object->page_name;
-$sect = $object->section_number;
-$com = $object->commentText;
-    //echo "Name:".$sect;
-	postComment($pg_name, $sect, $com);
+	//postComment(25);
 ?>
